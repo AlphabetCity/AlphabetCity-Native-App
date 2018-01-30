d5c399f49032fe0bac8ac2fdf177804cf677d51a
Daniela Tizon <dtizonportilla@gmail.com>
2018-01-30 18:56:03 -0500

    added basic Settings component

--- Router.js
+++ Router.js
@@ -1,20 +1,26 @@
 'use strict'
 import { StackNavigator } from 'react-navigation'
-import { Main, AR } from './components'
+import { Main, AR, Settings } from './components'
 
 const routes = {
   Main: {
     screen: Main,
     navigationOptions: {
       header: null
-    },
+    }
   },
   AR: {
     screen: AR,
     navigationOptions: {
       header: null
     }
-  }
+  },
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      header: null
+    }
+  },
 }
 
 const AppNavigator = StackNavigator(routes)
--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 'use strict'
 import Main from './Main'
 import AR from './AR'
+import Settings from './Settings'
 
-export { Main, AR }
+export { Main, AR, Settings }
