ec7fa257074757672564f4c99fa5f9fcb1c1e193
daniela_tizon <dtizonportilla@gmail.com>
2018-02-01 11:32:40 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 'use strict'
 import React, { Component } from 'react'
 import { Provider, connect } from 'react-redux'
-// import ProfileNavigator from './Router'
 import AppNavigator from './StackNav'
 import { addNavigationHelpers } from 'react-navigation'
 import { Main } from './components'
