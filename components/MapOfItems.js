1bb63f477e03ed6025a4021685800bccb92b92ca
Eetai Ben-Sasson <eeetai@gmail.com>
2018-02-03 14:12:34 -0500

    satchel redirect works

--- components/MapOfItems.js
+++ components/MapOfItems.js
@@ -1,3 +1,4 @@
+'use strict'
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { MapView } from 'expo'
@@ -5,31 +6,31 @@ import blurryPin from '../assets/blurryPin.png'
 
 const MapOfItems = (props) => {
 
-    return (
-      <View style={styles.container}>
-        <MapView
-          style={{ flex: 1 }}
-          initialRegion={props.initialRegion}
-        >
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={props.initialRegion}
+      >
         {/* current user's position */}
-          <MapView.Marker
-            coordinate={props.markerPosition}
-          />
+        <MapView.Marker
+          coordinate={props.markerPosition}
+        />
         {/* hidden items' positions */}
-          {
-            props.allHiddenItems.map(item => {
-              return (
-                <MapView.Marker
-                  key={item.id}
-                  coordinate={{ latitude: item.latitude, longitude: item.longitude }}
-                  image={blurryPin}
-                />
-              )
-            })
-          }
-        </MapView>
-      </View>
-    )
+        {
+          props.allHiddenItems.map(item => {
+            return (
+              <MapView.Marker
+                key={item.id}
+                coordinate={{ latitude: item.latitude, longitude: item.longitude }}
+                image={blurryPin}
+              />
+            )
+          })
+        }
+      </MapView>
+    </View>
+  )
 }
 
 
