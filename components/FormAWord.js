b68cb5f561d76d54bedc7d38828a2f00f0bc8531
DianaLease <DianaRLease@gmail.com>
2018-02-05 13:06:10 -0500

    updating satchel

---
+++ components/FormAWord.js
@@ -0,0 +1,7 @@
+'use strict'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { List, ListItem, ListView } from 'react-native-elements'
+import { View, Text } from 'react-native'
+
+
--- components/Satchel.js
+++ components/Satchel.js
@@ -2,34 +2,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { List, ListItem, ListView } from 'react-native-elements'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import { updateLetter } from '../store/satchel'
 
 class Satchel extends Component {
-  constructor(props) {
-    super(props)
-  }
 
   render() {
     return (
       <View>
         <List containerStyle={{ marginBottom: 20 }}>
           {
-            this.props.satchel.map((satchelLetter, idx) => (
+            this.props.satchel.map((satchelLetter) => (
               <ListItem
                 roundAvatar
                 avatar={{ uri: 'http://dreamicus.com/data/apple/apple-04.jpg' }}
                 key={satchelLetter.id}
-                title={(satchelLetter.letterCategory.name).toString()}
+                title={`Drop the letter ${satchelLetter.letterCategory.name}`}
                 onPress={() => {
                   this.props.updateLetter(satchelLetter.id, { latitude: this.props.userLocation.latitude, longitude: this.props.userLocation.longitude })
                   this.props.navigation.goBack()
-                }
+                  }
                 }
               />
             ))
           }
         </List>
+        <Button
+          onPress={() => this.props.navigation.navigate('FormAWord')}
+        />
       </View>
     )
   }
