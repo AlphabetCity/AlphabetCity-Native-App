0b389eda9babbdaf5f464de4c2b6311661c3a9e0
allenhj <allendotjohnson@gmail.com>
2018-02-18 09:40:18 -0500

    change readme

---
+++ Nav.js
@@ -0,0 +1,167 @@
+'use strict'
+import React from 'react'
+import { StackNavigator, DrawerNavigator, DrawerItems } from 'react-navigation'
+import { Main, ARContainer, Auth, UserHome, Highscores, UserWords, Update, SortableHand } from './components'
+import { View, Button, Image } from 'react-native'
+
+const Stack = {
+  Main: {
+    screen: Main,
+    navigationOptions: {
+      header: null,
+    }
+  },
+  AR: {
+    screen: ARContainer,
+    navigationOptions: {
+      header: null,
+    }
+  },
+  Auth: {
+    screen: Auth,
+    navigationOptions: {
+      title: 'Login or Sign up',
+      headerTintColor: '#706FD3',
+    }
+  },
+  Profile: {
+    screen: UserHome,
+    navigationOptions: ({navigation}) => ({
+      title: 'Profile',
+      headerTintColor: '#706FD3',
+      headerLeft: (
+        <Button
+          title="   Map"
+          color="#706FD3"
+          navigation={navigation}
+          onPress={
+            () => {navigation.navigate('Main')}
+          }
+        />
+      )
+    }),
+  },
+  Highscores: {
+    screen: Highscores,
+    navigationOptions: {
+      title: 'High Scores',
+      headerTintColor: '#706FD3',
+    }
+  },
+  SortableHand: {
+    screen: SortableHand,
+    navigationOptions: {
+      title: 'Make a Word',
+      headerTintColor: '#706FD3',
+    }
+  },
+  UserWords: {
+    screen: UserWords,
+    navigationOptions: {
+      title: 'My Words',
+      headerTintColor: '#706FD3',
+    }
+  },
+  UpdateUser: {
+    screen: Update,
+    navigationOptions: {
+      title: 'Update Profile',
+      headerTintColor: '#706FD3',
+    }
+  },
+}
+
+let loggedIn = true
+
+const DrawerUserRoutes = {
+  Main: {
+    screen: StackNavigator(Stack, { initialRouteName: 'Main' }),
+    navigationOptions: {
+      drawerLabel: 'Map',
+      header: null,
+    }
+  },
+  Profile: {
+    screen: StackNavigator(Stack, { initialRouteName: 'Profile' }),
+    navigationOptions: {
+      drawerLabel: 'Profile',
+      header: null,
+    }
+  },
+  Highscores: {
+    screen: StackNavigator(Stack, { initialRouteName: 'Highscores' }),
+    navigationOptions: {
+      drawerLabel: 'High Scores',
+      header: null,
+    }
+  },
+}
+
+const DrawerGuestRoutes = {
+  Login: {
+    screen: StackNavigator(Stack, { initialRouteName: 'Main' }),
+    navigationOptions: {
+      drawerLabel: 'Login',
+      header: null
+    }
+  },
+  Signup: {
+    screen: StackNavigator(Stack, { initialRouteName: 'Main' }),
+    navigationOptions: {
+      drawerLabel: 'Signup',
+      header: null
+    }
+  },
+}
+
+const styles = {
+  header: {
+    justifyContent: 'center',
+    backgroundColor: '#2C2C54',
+    height: 240,
+    alignItems: 'center',
+  },
+  logo: {
+    width: '90%',
+    height: '90%',
+    justifyContent: 'center',
+  },
+}
+
+const DrawerContent = (props) => (
+  <View
+    style={{
+      flex: 1,
+    }}>
+    <View style={styles.header}>
+      <Image
+        style={styles.logo}
+        source={require('./assets/icons/app-icon-transparent.png')}
+      />
+    </View>
+    <DrawerItems
+      {...props}
+      activeTintColor="#706fd3"
+      inactiveTintColor="#40407a"
+    />
+  </View>
+)
+
+let DrawerRoutes = loggedIn ? DrawerUserRoutes : DrawerGuestRoutes
+
+const AppNavigator = StackNavigator({
+  Drawer: {
+    name: 'Drawer',
+    screen: DrawerNavigator(DrawerRoutes,
+      {
+        contentComponent: DrawerContent,
+      }),
+  },
+  ...Stack
+},
+  {
+    headerMode: 'none'
+  }
+)
+
+export default AppNavigator
--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # Alphabet City
 
-## Concept
+Alphabet City is a location-based mobile game featuring augmented reality. Think Words with Friends meets Pokemon GO. Currently optimized for iPhone X only.
 
-A location-based augmented reality mobile app for iOS. Search for letters across your city and use them to form words and earn points.
+## Team
+
+- [@DianaLease](http://www.github.com/DianaLease)
+- [@eetai](http://www.github.com/eetai)
+- [@danielatp](http://www.github.com/danielatp)
+- [@allenhj](http://www.github.com/allenhj)
+
+## Game Logic
+
+Users play by wandering around their city to find letters they can pick up in AR. Once a user forms a valid word, they earn points and their word is added to the map their current location.
+
+Play against your friends to see who can rack up the most points and view each other's words in AR.
+
+## Technologies
+
+The Alphabet City app is built using React Native with the Expo SDK. We use React Navigation for navigation and Redux to store state, and Three.js and expo-three to render 3D letters, using @EvanBacon's method [found here](https://github.com/EvanBacon/expo-three-text)
+
+Our backend server is built using Node.js, Express, and Sequelize with PostgreSQL.
+
+## Thanks
+
+Special thanks to those who helped us make this project a reality:
+
+- [@jluria](http://www.github.com/jluria)
+- [@EvanBacon](http://www.github.com/EvanBacon)
+- [@omriBernstein](http://www.github.com/omriBernstein)
+- [@coreygreenwald](http://www.github.com/coreygreenwald)
+- [@azoitos](http://www.github.com/azoitos)
