554043ca969a1bbcd31dc4011e967fb385979f2e
allenhj <allendotjohnson@gmail.com>
2018-03-03 09:07:12 -0500

    make radius and fill opacity track point value

--- components/MapOfLetters.js
+++ components/MapOfLetters.js
@@ -3,8 +3,11 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { MapView } from 'expo'
 
-const MapOfLetters = (props) => {
+const RADIUS_SCALE = 5
+const FILL_SCALE = 0.2
+const OPACITY_REDUCTION = 20
 
+const MapOfLetters = props => {
   return (
     <View style={styles.container}>
       <MapView.Animated
@@ -23,34 +26,40 @@ const MapOfLetters = (props) => {
         }}
         onPress={props.hideDropDown}
       >
-        {/* hidden letters' positions */}
-        {
-          props.allHiddenLetters.map(letter => {
-            return (
-              <MapView.Circle
-                key={letter.id}
-                center={{ latitude: letter.latitude, longitude: letter.longitude }}
-                radius={10 * Math.random() * 3}
-                strokeWidth={0.5}
-                strokeColor={'rgba(112, 111, 211, 0.0)'}
-                fillColor={'rgba(112, 111, 211, 0.1)'}
-
-              />
-            )
-          })
-        }
+        {/* Hidden Letters' Positions
+            Center: offset a random amount in both directions so letter's
+              location is in random within the circle
+            Radius: equal to the point value of the letter in meters scaled
+              by some constant
+            Fill Opacity: Reduce opacity from 1 based on point value (lighter =
+              more points, darker = fewer points), scaled by a constant
+        */}
+        {props.allHiddenLetters.map(letter => {
+          return (
+            <MapView.Circle
+              key={letter.id}
+              center={{
+                latitude: letter.latitude,
+                longitude: letter.longitude
+              }}
+              radius={letter.letterCategory.points * RADIUS_SCALE}
+              strokeColor={'rgba(112, 111, 211, 0)'}
+              fillColor={`rgba(112, 111, 211, ${(1 -
+                letter.letterCategory.points / OPACITY_REDUCTION) *
+                FILL_SCALE})`}
+            />
+          )
+        })}
       </MapView.Animated>
     </View>
   )
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-end'
   }
 })
 
 export default MapOfLetters
-
