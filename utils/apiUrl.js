fa20122d4d654925741d597ec0f5c22829b4bd36
allenhj <allendotjohnson@gmail.com>
2018-04-20 13:55:41 -0700

    change to prod

--- utils/apiUrl.js
+++ utils/apiUrl.js
@@ -1,9 +1,9 @@
 // Environment config
 
 // production
-// const API_URL = 'https://alphabetcity.herokuapp.com'
+const API_URL = 'https://alphabetcity.herokuapp.com'
 
 // development
-const API_URL = 'http://192.168.0.183:8080'
+// const API_URL = 'http://YOUR_IP_ADDRESS:8080'
 
 export default API_URL
